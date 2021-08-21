617aa750b1ef6af8a7b05a6db5555852c2c537f2
sadrakmorais <sadraksdk1@gmail.com>
2021-08-21 14:09:01 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -67,6 +67,11 @@ export default ()=>{
      </div>
 
      </footer>
+     {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="loading"></img>
+      </div>
+    }
    </div>
 
   )
--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -4,25 +4,37 @@ import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default   ({title,items}) =>{
-  const [scrollX,setScrollX] = useState(0 )
+  const [scrollX,setScrollX] = useState(0)
 
   const handleLeftArrow = ()=>{
+    let x = scrollX + Math.round(window.innerWidth / 2)
+    if(x > 0){
+      x = 0
+    }
+    setScrollX(x)
   }
 
-  const handleRightArrow = ()=>{
+  const handleRightArrow = () =>{
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = items.results.length * 150;
+    if(window.innerWidth - listW > x)
+    {
+        x = (window.innerWidth - listW) - 60;
+    }
+    setScrollX(x);
+}
 
-  }
   return(
     <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movieRow-left" onCick={handleLeftArrow}>
+      <div className="movieRow-left" onClick={handleLeftArrow}>
           <NavigateBeforeIcon style={{fontSize:50}}/>
       </div>
-      <div className="movieRow-right">
+      <div className="movieRow-right" onClick={handleRightArrow}>
           <NavigateNextIcon style={{fontSize:50}}/>
       </div>
 
-      <div className="movieRow-listArea" onCick={handleRightArrow}>
+      <div className="movieRow-listArea">
         <div className="movieRow-list" style={{
           marginLeft: scrollX,
           width: items.results.length *150
