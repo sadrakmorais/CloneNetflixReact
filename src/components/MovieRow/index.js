75fc4ec3865cf9b57399b4eb5bb3030ffc0b4096
sadrakmorais <sadraksdk1@gmail.com>
2021-08-21 13:08:09 -0300

    AttLista

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MovieRow.css'
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default   ({title,items}) =>{
+  const [scrollX,setScrollX] = useState(0 )
+
+  const handleLeftArrow = ()=>{
+  }
+
+  const handleRightArrow = ()=>{
+
+  }
   return(
     <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movieRow-left">
+      <div className="movieRow-left" onCick={handleLeftArrow}>
           <NavigateBeforeIcon style={{fontSize:50}}/>
       </div>
       <div className="movieRow-right">
           <NavigateNextIcon style={{fontSize:50}}/>
       </div>
 
-      <div className="movieRow-listArea">
-        <div className="movieRow-list">
+      <div className="movieRow-listArea" onCick={handleRightArrow}>
+        <div className="movieRow-list" style={{
+          marginLeft: scrollX,
+          width: items.results.length *150
+          }}>
         {items.results.length > 0 && items.results.map((item,key)=>(
           <div key={key} className="movieRow-item">
            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
